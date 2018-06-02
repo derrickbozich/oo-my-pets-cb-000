639e6cdf94cc35cbe5c7fc9ebd9a9437999ba0ce
derrick b <dbozich1@gmail.com>
2018-06-02 05:04:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,12 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}  
+  end
+
+  def add_pet(pet)
+    @pets.push(self.pets)
+  end
 end
